7b00d745a909caea5a6bd39cee05a64e92062521
Mohammed Shahrukh <mohammedshahrukhalam@gmail.com>
2025-12-16 19:16:15 +0530

    Code changes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
   import Navbar from './components/Navbar.vue';
+  import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Navbar/>
   <h1>Hello Vue</h1>
+  <Footer/>
 </template>
